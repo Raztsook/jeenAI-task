d71a4526789a0c7ce76ad4ef3a0729286801f403
Raz Tsook <raz@tsook.net>
2025-01-27 15:18:29 +0300

    Updated Procfile for Railway deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,17 @@ import os
 from flask import Flask
 from .routes import bp
 
-def create_app():
+def create_app(*args, **kwargs):
     app = Flask(__name__)
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
     app.config['SECRET_KEY'] = 'raztsook1996jeenproject'
 
-    # ודא שתיקיית UPLOADS קיימת
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
         print(f"Created uploads directory: {app.config['UPLOAD_FOLDER']}")
 
-    # רישום ה-Blueprint
+    from .routes import bp
     app.register_blueprint(bp)
 
     return app
+
